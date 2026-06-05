4126f96bc476ab3d0e05a97b975d9ec465a94988
Qimin Zhang <zqm15163827857@gmail.com>
2026-06-05 21:37:10 +0000

    week1 pipeline

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import click
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+@click.command()
+@click.option('--pg-user', type=str, default='root', help='PostgreSQL username')
+@click.option('--pg-pass', type=str, default='root', help='PostgreSQL password')
+@click.option('--pg-host', type=str, default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', type=str, default='5432', help='PostgreSQL port')
+@click.option('--pg-db', type=str, default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', type=int, default=2021, help='Year of taxi data')
+@click.option('--month', type=int, default=1, help='Month of taxi data (1-12)')
+@click.option('--target-table', type=str, default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', type=int, default=100000, help='Number of rows per chunk')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+        if first:
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists='replace'
+            )
+            first = False
+
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists='append'
+        )
+
+if __name__ == '__main__':
+    run()
